8264058d7412b9faad29ea40c95a2e0ba8f83fb8
Murthy-GM <murthy19497@gmail.com>
2024-02-05 11:22:09 +0530

    task code'

--- src/components/Login/Login-page.js
+++ src/components/Login/Login-page.js
@@ -1,60 +1,58 @@
+import React, { useState } from "react";
 
-
-
-
-
-
-import React, { useState } from 'react';
-
-import './Login-page.css'
+import "./Login-page.css";
 
 const YourFormComponent = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    
-    if (username.trim() === '' || password.trim() === '') {
-      alert('Please fill in all fields');
+    if (username.trim() === "" || password.trim() === "") {
+      alert("Please fill in all fields");
       return;
     }
-    const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&*()_+!])[A-Za-z\d@$%^&*()_+!]{8,}$/;
+    const passwordRegex =
+      /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&*()_+!])[A-Za-z\d@$%^&*()_+!]{8,}$/;
     if (!passwordRegex.test(password)) {
-      alert('Password must contain at least 8 characters, one uppercase letter, one lowercase letter, one special character, and one number.');
+      alert(
+        "Password must contain at least 8 characters, one uppercase letter, one lowercase letter, one special character, and one number."
+      );
       return;
     }
-    
+
     window.location.href = "/booking";
   };
 
   return (
-    <form className='form-page' onSubmit={handleSubmit}>
+    <form className="form-page" onSubmit={handleSubmit}>
       <div className="form-container">
-        <label className='label-text'>
+        <label className="label-text">
           Enter Username :
           <input
-            className='input-text'
-            style={{ marginLeft: '0.3rem' }}
+            className="input-text"
+            style={{ marginLeft: "0.3rem" }}
             type="text"
             placeholder="Enter Username"
             value={username}
             onChange={(e) => setUsername(e.target.value)}
           />
         </label>
-        <label className='label-text'>
+        <label className="label-text">
           Enter Password :
           <input
-            className='input-text'
-            style={{ marginLeft: '0.3rem' }}
+            className="input-text"
+            style={{ marginLeft: "0.3rem" }}
             type="password"
             placeholder="Enter Password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
         </label>
-        <button type="submit" className='btn'>Submit</button>
+        <button type="submit" className="btn">
+          Submit
+        </button>
       </div>
     </form>
   );
